4ec594be5cd5bb38dae15913ad8b270ae88113ae
tigercosmos <phy.tiger@gmail.com>
2018-01-24 17:34:25 +0800

    add humidity

--- 183/make_ntu_obs_json.php
+++ 183/make_ntu_obs_json.php
@@ -36,17 +36,23 @@ if (count($data["rain_24"]) >= 24) {
     array_shift($data["rain_24"]);
 }
 
+if (count($data["humd_24"]) >= 24) {
+    array_shift($data["humd_24"]);
+}
+
 $new_data['press_24'] = $data["press_24"];
 $new_data['temp_24'] = $data["temp_24"];
 $new_data['wind_dir_24'] = $data["wind_dir_24"];
 $new_data['wind_speed_24'] = $data["wind_speed_24"];
 $new_data['rain_24'] = $data["rain_24"];
+$new_data['humd_24'] = $data["humd_24"];
 
 array_push($new_data["press_24"], $new_data['DD_MMP2']);
 array_push($new_data["temp_24"], $new_data['DD_T']);
 array_push($new_data["wind_dir_24"], $new_data['DD_10D']);
 array_push($new_data["wind_speed_24"], $new_data['DD_F10']);
 array_push($new_data["rain_24"], $new_data['DD_R']);
+array_push($new_data["humd_24"], $new_data['DD_RH']);
 
 $txt = json_encode($new_data);
 echo $txt;
